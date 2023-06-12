387b6c4f0193e210b6186e33d6888d669141c0bf
Zin <109268611+Skyrim626@users.noreply.github.com>
2023-06-12 17:49:27 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once('db.php');
+include_once('request.php');
+include('cors.php');
+
+$requestType = $_SERVER['REQUEST_METHOD'];
+
+if (!check("POST", $requestType)) {
+    echo "Error: 403 Forbidden Access";
+    exit();
+}
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+/* $sql = 'SELECT * FROM users WHERE username="'.$data["username"].'" AND password="'.$data["password"].'"'; */
+$sql = 'SELECT * FROM admins WHERE username = "' . $data["username"] . '" AND password = "' . $data["password"] . '"';
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+    echo json_encode(["status" => true, "message" => "account found"]);
+} else {
+    echo json_encode(["status" => false, "message" => "account not found"]);
+}
